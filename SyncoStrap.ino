0fc0f8bfe84be5499c33878211dc33f45ac1c959
dea-turashvili <119975469+dea-turashvili@users.noreply.github.com>
2022-12-06 16:19:44 -0500

    Add files via upload

--- SyncoStrap.ino
+++ SyncoStrap.ino
@@ -3,7 +3,7 @@ SyncoStrap.ino
 This code is compatible with Arduino Uno, MPU6050 (Accelerometer), 
 AD8232 (ECG Sensor), and DHT11(Temp and Humidity Sensor). It contains an
 algorithm that can determine whether or not someone is at risk of fainting.
-Contributers include Esha Vaishnav, Yousuf Shehadi, Ruhaan Baghaat, and Dea Turashvili
+Contributers include Esha Vaishnav, Yousuf Shehadi, Ruhaan Baghat, and Dea Turashvili
 
 ******************************************************************************/
 #include <Wire.h>
